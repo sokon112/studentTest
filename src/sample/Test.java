7ce5003b59b6f6ec802e1c78d0de3811df5c7926
KimSeokHyun <sokon112@gmail.com>
2021-04-29 19:33:28 +0900

    솔데스크 29일차 새롭게 시작

---
+++ src/sample/Test.java
@@ -0,0 +1,21 @@
+package sample;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+
+
+public class Test {
+
+	private String name;
+	private int age;
+	private String addr;
+	private char gender;
+	
+	
+	
+}
